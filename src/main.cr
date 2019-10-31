55660ba5880f5525b66900e40365264b04146191
Juan Wajnerman <jwajnerman@manas.com.ar>
2019-10-31 18:58:18 -0300

    Run only once (no loop)

--- src/main.cr
+++ src/main.cr
@@ -4,37 +4,32 @@ require "logger"
 OUTPUT = ENV["OUTPUT"]? || "hostnames.json"
 
 log = Logger.new(STDOUT)
-previous_results = nil
 
-loop do
-  begin
-    Rancher::Client.new do |client|
-      projects = client.list_projects
+begin
+  Rancher::Client.new do |client|
+    projects = client.list_projects
 
-      results = projects.data.each
-        .reject { |p| p.state == "inactive" }
-        .map do |p|
-          domains = p.load_balancer_services
-            .data
-            .flat_map(&.lb_config.port_rules)
-            .map(&.hostname?)
-            .compact_map(&.itself)
-            .reject(&.empty?)
-            .reject(&.starts_with?("*"))
-            .uniq
-          {labels: {env: p.name}, targets: domains}
-        end
-        .to_a
-
-      if results != previous_results
-        log.info "Writing results to #{OUTPUT}"
-        File.write OUTPUT, results.to_json
-        previous_results = results
+    results = projects.data.each
+      .reject { |p| p.state == "inactive" }
+      .map do |p|
+        domains = p.load_balancer_services
+          .data
+          .flat_map(&.lb_config.port_rules)
+          .map(&.hostname?)
+          .compact_map(&.itself)
+          .reject(&.empty?)
+          .reject(&.starts_with?("*"))
+          .uniq
+        {labels: {env: p.name}, targets: domains}
       end
-    end
-  rescue ex
-    log.error ex
-  end
+      .to_a
 
-  sleep 60
+    results_json = results.to_json
+    unless File.exists?(OUTPUT) && File.read(OUTPUT) == results_json
+      log.info "Writing results to #{OUTPUT}"
+      File.write OUTPUT, results_json
+    end
+  end
+rescue ex
+  log.error ex
 end
